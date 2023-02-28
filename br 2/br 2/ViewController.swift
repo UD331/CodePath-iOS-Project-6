0bea78727daabd6e71498773b55e93a333a90ab9
Udayan Pandey <pandeyudyan@gmail.com>
2023-02-28 18:17:38 -0500

    Project6

---
+++ br 2/br 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  br 2
+//
+//  Created by Udayan Pandey on 2/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
